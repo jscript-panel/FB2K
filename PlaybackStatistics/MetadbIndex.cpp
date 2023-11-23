c2790090e9065a020373cbec6d45cba745f8bfb1
marc2k3 <marc2003@live.co.uk>
2023-11-23 23:57:17 +0000

    Create MetadbIndex.cpp

---
+++ PlaybackStatistics/MetadbIndex.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.hpp"
+
+MetadbIndex::MetadbIndex() : m_hasher(hasher_md5::get())
+{
+	titleformat_compiler::get()->compile_safe(m_obj, Component::pin_to.get());
+}
+
+MetadbIndex* MetadbIndex::client()
+{
+	static MetadbIndex* cached = new service_impl_single_t<MetadbIndex>();
+	return cached;
+}
+
+void MetadbIndex::init()
+{
+	auto api = PlaybackStatistics::api();
+
+	try
+	{
+		api->add(client(), guids::metadb_index, system_time_periods::week * 4);
+		api->dispatch_global_refresh();
+	}
+	catch (const std::exception& e)
+	{
+		api->remove(guids::metadb_index);
+		FB2K_console_formatter() << fmt::format("{}: Playback Statistics critical initialisation failure: {}", Component::name, e.what());
+	}
+}
+
+metadb_index_hash MetadbIndex::transform(const file_info& info, const playable_location& location)
+{
+	string8 str;
+	m_obj->run_simple(location, &info, str);
+	return m_hasher->process_single_string(str).xorHalve();
+}

b686029a13b6cac7535cfa1f3aa4cec08be7b2c5
marc2k3 <marc2003@live.co.uk>
2023-12-09 13:23:20 +0000

    Update TitleFormatHook

--- TitleFormatHook.cpp
+++ TitleFormatHook.cpp
@@ -1,53 +1,91 @@
 #include "stdafx.hpp"
 #include "TitleFormatHook.hpp"
 
+#include "CountryFlag.hpp"
+
+bool TitleFormatHook::process_country_flag(titleformat_text_out* out, const char* func, titleformat_hook_function_params* params, bool& found_flag)
+{
+	if (!compare_string(func, "country_flag")) return false;
+
+	if (m_param_count == 1)
+	{
+		const auto country_or_code = get_string(params, 0);
+		const auto flag_code = CountryFlag::get(country_or_code);
+		found_flag = flag_code.length() > 0;
+
+		if (found_flag)
+		{
+			out->write(titleformat_inputtypes::unknown, flag_code.c_str());
+		}
+	}
+
+	return true;
+}
+
 bool TitleFormatHook::process_field(titleformat_text_out*, const char*, size_t, bool& found_flag)
 {
 	found_flag = false;
 	return false;
 }
 
-bool TitleFormatHook::process_function(titleformat_text_out* out, const char* name, size_t, titleformat_hook_function_params* params, bool& found_flag)
+bool TitleFormatHook::process_font(titleformat_text_out* out, const char* func, titleformat_hook_function_params* params, bool& found_flag)
 {
-	found_flag = false;
-	if (stricmp_utf8(name, "font") != 0) return false;
+	if (!compare_string(func, "font")) return false;
 
 	static constexpr fmt::string_view bel = "\x7";
 	static constexpr fmt::string_view tab = "\t";
-	const size_t count = params->get_param_count();
 
-	if (count == 0)
+	if (m_param_count == 0)
 	{
 		found_flag = true;
 		out->write(titleformat_inputtypes::unknown, fmt::format("{0}{0}", bel).c_str());
 	}
-	else if (count >= 2 && count <= 6)
+	else if (m_param_count >= 2 && m_param_count <= 6)
 	{
-		found_flag = true;
-
-		const auto get_value_checked = [params, count](size_t index, size_t min, size_t max, size_t fallback)
-			{
-				if (index < count)
-				{
-					const size_t value = params->get_param_uint(index);
-					if (value >= min && value <= max) return value;
-				}
-				return fallback;
-			};
-
-		const char* font;
-		size_t font_length{};
-		params->get_param(0, font, font_length);
-
-		const size_t size = get_value_checked(1, 8, 144, 12);
-		const size_t weight = get_value_checked(2, 100, 950, 400);
-		const size_t style = get_value_checked(3, 0, 2, 0);
-		const size_t underline = get_value_checked(4, 0, 1, 0);
-		const size_t strikethrough = get_value_checked(5, 0, 1, 0);
+		const auto name = get_string(params, 0);
+		const auto size = get_num(params, 1, 8, 144, 12);
+		const auto weight = get_num(params, 2, 100, 950, 400);
+		const auto style = get_num(params, 3, 0, 2, 0);
+		const auto underline = get_num(params, 4, 0, 1, 0);
+		const auto strikethrough = get_num(params, 5, 0, 1, 0);
 		const std::array nums = { size, weight, style, underline, strikethrough };
 
-		out->write(titleformat_inputtypes::unknown, fmt::format("{}{}{}{}{}", bel, font, tab, fmt::join(nums, tab), bel).c_str());
+		found_flag = true;
+		out->write(titleformat_inputtypes::unknown, fmt::format("{}{}{}{}{}", bel, name, tab, fmt::join(nums, tab), bel).c_str());
 	}
 
 	return true;
+}
+
+bool TitleFormatHook::process_function(titleformat_text_out* out, const char* func, size_t, titleformat_hook_function_params* params, bool& found_flag)
+{
+	found_flag = false;
+	m_param_count = params->get_param_count();
+
+	if (process_font(out, func, params, found_flag)) return true;
+	if (process_country_flag(out, func, params, found_flag)) return true;
+	return false;
+}
+
+std::string TitleFormatHook::get_string(titleformat_hook_function_params* params, size_t index)
+{
+	if (index < m_param_count)
+	{
+		const char* str;
+		size_t len{};
+		params->get_param(index, str, len);
+
+		return std::string(str, len);
+	}
+	return std::string();
+}
+
+size_t TitleFormatHook::get_num(titleformat_hook_function_params* params, size_t index, size_t min, size_t max, size_t fallback)
+{
+	if (index < m_param_count)
+	{
+		const auto value = params->get_param_uint(index);
+		if (value >= min && value <= max) return value;
+	}
+	return fallback;
 }
--- TitleFormatHook.hpp
+++ TitleFormatHook.hpp
@@ -4,5 +4,13 @@ class TitleFormatHook : public titleformat_hook
 {
 public:
 	bool process_field(titleformat_text_out*, const char*, size_t, bool& found_flag) final;
-	bool process_function(titleformat_text_out* out, const char* name, size_t, titleformat_hook_function_params* params, bool& found_flag) final;
+	bool process_function(titleformat_text_out* out, const char* func, size_t, titleformat_hook_function_params* params, bool& found_flag) final;
+
+private:
+	bool process_font(titleformat_text_out* out, const char* func, titleformat_hook_function_params* params, bool& found_flag);
+	bool process_country_flag(titleformat_text_out* out, const char* func, titleformat_hook_function_params* params, bool& found_flag);
+	std::string get_string(titleformat_hook_function_params* params, size_t index);
+	size_t get_num(titleformat_hook_function_params* params, size_t index, size_t min, size_t max, size_t fallback);
+
+	size_t m_param_count{};
 };

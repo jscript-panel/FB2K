db7640bad7ff55bc3ae7def0b3b6b94a9d7622ee
marc2k3 <marc2003@live.co.uk>
2024-03-24 11:27:48 +0000

    AlbumArtStatic::attach_image2(

--- AlbumArt/AlbumArtStatic.cpp
+++ AlbumArt/AlbumArtStatic.cpp
@@ -7,10 +7,79 @@ GUID AlbumArtStatic::get_guid(size_t id)
 	return *guids::art[id];
 }
 
+HRESULT AlbumArtStatic::bitmap_to_jpg_data(IWICBitmap* bitmap, album_art_data_ptr& data)
+{
+	D2D1_SIZE_U size{};
+	RETURN_IF_FAILED(bitmap->GetSize(&size.width, &size.height));
+	auto rect = to_WICRect(size);
+
+	wil::com_ptr_t<IStream> stream;
+	wil::com_ptr_t<IWICBitmapEncoder> encoder;
+	wil::com_ptr_t<IWICBitmapFrameEncode> frame_encode;
+	wil::com_ptr_t<IWICStream> wic_stream;
+
+	RETURN_IF_FAILED(CreateStreamOnHGlobal(nullptr, TRUE, &stream));
+	RETURN_IF_FAILED(factory::imaging->CreateStream(&wic_stream));
+	RETURN_IF_FAILED(wic_stream->InitializeFromIStream(stream.get()));
+	RETURN_IF_FAILED(factory::imaging->CreateEncoder(GUID_ContainerFormatJpeg, nullptr, &encoder));
+	RETURN_IF_FAILED(encoder->Initialize(stream.get(), WICBitmapEncoderNoCache));
+	RETURN_IF_FAILED(encoder->CreateNewFrame(&frame_encode, nullptr));
+	RETURN_IF_FAILED(frame_encode->Initialize(nullptr));
+	RETURN_IF_FAILED(frame_encode->SetSize(size.width, size.height));
+	RETURN_IF_FAILED(frame_encode->WriteSource(bitmap, &rect));
+	RETURN_IF_FAILED(frame_encode->Commit());
+	RETURN_IF_FAILED(encoder->Commit());
+
+	HGLOBAL hg{};
+	RETURN_IF_FAILED(GetHGlobalFromStream(stream.get(), &hg));
+	auto image = wil::unique_hglobal_locked(hg);
+	auto gsize = GlobalSize(image.get());
+	data = album_art_data_impl::g_create(image.get(), gsize);
+
+	return S_OK;
+}
+
+HRESULT AlbumArtStatic::bitmap_to_webp_data(IWICBitmap* bitmap, album_art_data_ptr& data)
+{
+	D2D1_SIZE_U size{};
+	RETURN_IF_FAILED(bitmap->GetSize(&size.width, &size.height));
+	auto rect = to_WICRect(size);
+
+	uint8_t* ptr{};
+	uint8_t* output{};
+	uint32_t dummy{}, stride{};
+	wil::com_ptr_t<IWICBitmapLock> lock;
+
+	RETURN_IF_FAILED(bitmap->Lock(&rect, WICBitmapLockWrite, &lock));
+	RETURN_IF_FAILED(lock->GetDataPointer(&dummy, &ptr));
+	RETURN_IF_FAILED(lock->GetStride(&stride));
+
+	const size_t new_size = WebPEncodeBGRA(ptr, rect.Width, rect.Height, static_cast<int>(stride), 95.f, &output);
+	if (new_size == 0) return E_FAIL;
+
+	data = album_art_data_impl::g_create(output, new_size);
+	WebPFree(output);
+
+	return S_OK;
+}
+
 HRESULT AlbumArtStatic::check_id(size_t id)
 {
 	if (id < guids::art.size()) return S_OK;
 	return E_INVALIDARG;
+}
+
+HRESULT AlbumArtStatic::image_to_data(IJSImage* image, Type type, album_art_data_ptr& data)
+{
+	IWICBitmap* bitmap{};
+	RETURN_IF_FAILED(image->get(arg_helper(&bitmap)));
+
+	if (type == Type::JPG)
+	{
+		return bitmap_to_jpg_data(bitmap, data);
+	}
+
+	return bitmap_to_webp_data(bitmap, data);
 }
 
 HRESULT AlbumArtStatic::to_bitmap(const album_art_data_ptr& data, wil::com_ptr_t<IWICBitmap>& bitmap)
@@ -64,11 +133,21 @@ void AlbumArtStatic::attach_image(metadb_handle_list_cref handles, size_t id, wi
 {
 	const GUID guid = get_guid(id);
 	auto data = path_to_data(path);
-	if (data.is_valid())
-	{
-		auto callback = fb2k::service_new<Attach>(Attach::Action::Attach, handles, guid, data);
-		Attach::init(callback, "Attaching image...");
-	}
+	if (data.is_empty()) return;
+	
+	auto callback = fb2k::service_new<Attach>(Attach::Action::Attach, handles, guid, data);
+	Attach::init(callback, "Attaching image...");
+}
+
+void AlbumArtStatic::attach_image2(metadb_handle_list_cref handles, size_t id, Type type, IJSImage* image)
+{
+	const GUID guid = get_guid(id);
+	album_art_data_ptr data;
+
+	if FAILED(image_to_data(image, type, data)) return;
+
+	auto callback = fb2k::service_new<Attach>(Attach::Action::Attach, handles, guid, data);
+	Attach::init(callback, "Attaching image...");
 }
 
 void AlbumArtStatic::remove_attached_image(metadb_handle_list_cref handles, size_t id)
--- AlbumArt/AlbumArtStatic.hpp
+++ AlbumArt/AlbumArtStatic.hpp
@@ -3,12 +3,22 @@
 class AlbumArtStatic
 {
 public:
+	enum class Type
+	{
+		JPG,
+		WEBP,
+	};
+
 	static GUID get_guid(size_t id);
+	static HRESULT bitmap_to_jpg_data(IWICBitmap* bitmap, album_art_data_ptr& data);
+	static HRESULT bitmap_to_webp_data(IWICBitmap* bitmap, album_art_data_ptr& data);
 	static HRESULT check_id(size_t id);
+	static HRESULT image_to_data(IJSImage* image, Type type, album_art_data_ptr& data);
 	static HRESULT to_bitmap(const album_art_data_ptr& data, wil::com_ptr_t<IWICBitmap>& bitmap);
 	static IJSImage* get_attached_image(const metadb_handle_ptr& handle, size_t id);
 	static album_art_data_ptr path_to_data(wil::zwstring_view path);
 	static void attach_image(metadb_handle_list_cref handles, size_t id, wil::zwstring_view path);
+	static void attach_image2(metadb_handle_list_cref handles, size_t id, Type type, IJSImage* image);
 	static void remove_attached_image(metadb_handle_list_cref handles, size_t id);
 	static void remove_all_attached_images(metadb_handle_list_cref handles);
 };

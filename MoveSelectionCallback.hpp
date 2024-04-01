112b46b79bddc70b247fdbb26bb3306f68d336db
marc2k3 <marc2003@live.co.uk>
2024-04-01 21:27:10 +0100

    Update MoveSelectionCallback.hpp

--- MoveSelectionCallback.hpp
+++ MoveSelectionCallback.hpp
@@ -3,33 +3,34 @@
 class MoveSelectionCallback : public playlist_manager::enum_items_callback
 {
 public:
-	MoveSelectionCallback(size_t new_pos) : m_new_pos(new_pos) {}
+	MoveSelectionCallback(size_t pos, size_t count) : m_pos(std::min(pos, count))
+	{
+		m_order.reserve(count);
+	}
 
 	bool on_item(size_t index, const metadb_handle_ptr&, bool selected) final
 	{
 		if (selected)
 		{
-			if (index < m_new_pos) m_selected_before++;
 			m_selected.emplace_back(index);
 		}
 		else
 		{
-			m_not_selected.emplace_back(index);
+			if (index < m_pos) m_not_selected_before.emplace_back(index);
+			else m_not_selected_after.emplace_back(index);
 		}
 		return true;
 	}
 
 	bool reorder(size_t playlistIndex)
 	{
-		m_new_pos = std::min(m_new_pos - m_selected_before, m_not_selected.size());
-		std::vector<size_t> new_order;
-		std::ranges::copy(m_not_selected | std::views::take(m_new_pos), std::back_inserter(new_order));
-		std::ranges::copy(m_selected, std::back_inserter(new_order));
-		std::ranges::copy(m_not_selected | std::views::drop(m_new_pos), std::back_inserter(new_order));
-		return Plman::api()->playlist_reorder_items(playlistIndex, new_order.data(), new_order.size());
+		m_order.append_range(m_not_selected_before);
+		m_order.append_range(m_selected);
+		m_order.append_range(m_not_selected_after);
+		return Plman::api()->playlist_reorder_items(playlistIndex, m_order.data(), m_order.size());
 	}
 
 private:
-	std::vector<size_t> m_selected, m_not_selected;
-	size_t m_new_pos{}, m_selected_before{};
+	std::vector<size_t> m_selected, m_not_selected_before, m_not_selected_after, m_order;
+	size_t m_pos{};
 };

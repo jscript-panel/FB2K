6ef523ec424de20454efeb0dee00399e374d8c3b
marc2k3 <marc2003@live.co.uk>
2024-04-11 10:48:49 +0100

    Tidy up SearchFilter

--- SearchFilter.cpp
+++ SearchFilter.cpp
@@ -10,44 +10,16 @@ metadb_handle_list SearchFilter::get_library_items(wil::zwstring_view query)
 	auto filter = get_filter(query);
 	if (filter.is_empty()) return items;
 
-	if (Fb::is_v1()) return get_items_legacy(filter, items);
-
-	auto index = search_index_manager::get()->get_library_index();
-	return get_items_from_index(filter, index);
+	return get_items(filter, items);
 }
 
 metadb_handle_list SearchFilter::get_items(const search_filter_v2::ptr& filter, metadb_handle_list_cref handles)
-{
-	if (Fb::is_v1()) return get_items_legacy(filter, handles);
-
-	auto index = search_index_manager::get()->create_index(handles, nullptr);
-	return get_items_from_index(filter, index);
-}
-
-metadb_handle_list SearchFilter::get_items_from_index(const search_filter_v2::ptr& filter, const search_index::ptr& index)
-{
-	auto arr = index->search(filter, nullptr, 0, fb2k::noAbort);
-	return arr->as_list_of<metadb_handle>();
-}
-
-metadb_handle_list SearchFilter::get_items_legacy(const search_filter_v2::ptr& filter, metadb_handle_list_cref handles)
 {
 	metadb_handle_list items(handles);
 	auto mask = js::pfc_array<bool>(items.get_count());
 	filter->test_multi(items, mask.get_ptr());
 	items.filter_mask(mask.get_ptr());
 	return items;
-}
-
-metadb_handle_list SearchFilter::get_playlist_items(size_t playlistIndex, wil::zwstring_view query)
-{
-	auto items = Plman::get_all_items(playlistIndex);
-	if (query.empty()) return items;
-
-	auto filter = get_filter(query);
-	if (filter.is_empty()) return items;
-
-	return get_items_legacy(filter, items);
 }
 
 search_filter_v2::ptr SearchFilter::get_filter(wil::zwstring_view query)

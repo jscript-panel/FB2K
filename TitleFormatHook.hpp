a78fad6f1c5ed733e7921edc925b7fbbcd5dc5fe
marc2k3 <marc2003@live.co.uk>
2024-04-18 20:23:45 +0100

    Update TitleFormatHook.hpp

--- TitleFormatHook.hpp
+++ TitleFormatHook.hpp
@@ -9,8 +9,8 @@ public:
 	bool process_function(titleformat_text_out* out, const char* func, size_t, titleformat_hook_function_params* params, bool& found_flag) final;
 
 private:
-	bool process_font(titleformat_text_out* out, const char* func, titleformat_hook_function_params* params, bool& found_flag);
 	bool process_country_flag(titleformat_text_out* out, const char* func, titleformat_hook_function_params* params, bool& found_flag);
+	bool process_font(titleformat_text_out* out, const char* func, titleformat_hook_function_params* params, bool& found_flag);
 	bool process_since(titleformat_text_out* out, const char* func, titleformat_hook_function_params* params, bool& found_flag);
 	std::string get_string(titleformat_hook_function_params* params, size_t index);
 	size_t get_num(titleformat_hook_function_params* params, size_t index, size_t min, size_t max, size_t fallback);

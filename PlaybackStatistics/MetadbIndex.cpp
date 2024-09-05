9cafdc6bf9c7e519d595e2f205244f4c90eb2df1
marc2k3 <marc2003@live.co.uk>
2024-09-05 17:58:43 +0100

    Move cfg

--- PlaybackStatistics/MetadbIndex.cpp
+++ PlaybackStatistics/MetadbIndex.cpp
@@ -1,8 +1,33 @@
 #include "stdafx.hpp"
 
+namespace
+{
+	static constexpr std::string_view default_pin_to = "$lower($meta(artist,0) - %title%)";
+	cfg_string pin_to(guids::cfg_string_pin_to, default_pin_to.data());
+
+	advconfig_branch_factory advconfig_branch(Component::name.data(), guids::advconfig_branch, advconfig_branch::guid_branch_tools, 0.0);
+	advconfig_string_factory advconfig_pin_to("Playback Statistics Title Format", guids::advconfig_pin_to, guids::advconfig_branch, 0.0, default_pin_to.data(), preferences_state::needs_restart);
+
+	void init_stage()
+	{
+		const auto adv_pin_to = advconfig_pin_to.get();
+
+		if (adv_pin_to != pin_to)
+		{
+			// If pattern has changed, nuke old data before GUID is registered
+			pin_to = adv_pin_to;
+			PlaybackStatistics::api()->erase_orphaned_data(guids::metadb_index);
+		}
+
+		MetadbIndex::init();
+	}
+
+	FB2K_ON_INIT_STAGE(init_stage, init_stages::after_config_read)
+}
+
 MetadbIndex::MetadbIndex() : m_hasher(hasher_md5::get())
 {
-	titleformat_compiler::get()->compile_safe(m_obj, Component::pin_to.get());
+	titleformat_compiler::get()->compile_safe(m_obj, pin_to.get());
 }
 
 #pragma region static
@@ -35,26 +60,4 @@ metadb_index_hash MetadbIndex::transform(const file_info& info, const playable_l
 	string8 str;
 	m_obj->run_simple(location, &info, str);
 	return m_hasher->process_single_string(str).xorHalve();
-}
-
-namespace
-{
-	advconfig_branch_factory advconfig_branch(Component::name.data(), guids::advconfig_branch, advconfig_branch::guid_branch_tools, 0.0);
-	advconfig_string_factory advconfig_pin_to("Playback Statistics Title Format", guids::advconfig_pin_to, guids::advconfig_branch, 0.0, "$lower($meta(artist,0) - %title%)", preferences_state::needs_restart);
-
-	void init_stage()
-	{
-		const auto adv_pin_to = advconfig_pin_to.get();
-
-		if (adv_pin_to != Component::pin_to)
-		{
-			// If pattern has changed, nuke old data before GUID is registered
-			Component::pin_to = adv_pin_to;
-			PlaybackStatistics::api()->erase_orphaned_data(guids::metadb_index);
-		}
-
-		MetadbIndex::init();
-	}
-
-	FB2K_ON_INIT_STAGE(init_stage, init_stages::after_config_read)
 }

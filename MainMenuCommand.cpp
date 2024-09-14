da734f785af8be0e8a2603ae4d9927a66100dbb4
marc2k3 <marc2003@live.co.uk>
2024-09-14 10:17:48 +0100

    Update MainMenuCommand.cpp

--- MainMenuCommand.cpp
+++ MainMenuCommand.cpp
@@ -32,7 +32,7 @@ std::string MainMenuCommand::build_parent_path(GUID parent)
 
 	while (parent != pfc::guid_null)
 	{
-		mainmenu_group::ptr group_ptr = group_guid_map[parent];
+		const auto group_ptr = group_guid_map[parent];
 		mainmenu_group_popup::ptr group_popup_ptr;
 
 		if (group_ptr->cast(group_popup_ptr))
@@ -59,13 +59,13 @@ bool MainMenuCommand::execute()
 		mainmenu_commands_v2::ptr v2_ptr;
 		ptr->cast(v2_ptr);
 
-		const std::string parent_path = build_parent_path(ptr->get_parent());
+		const auto parent_path = build_parent_path(ptr->get_parent());
 
 		for (const uint32_t i : std::views::iota(0U, ptr->get_command_count()))
 		{
 			if (v2_ptr.is_valid() && v2_ptr->is_command_dynamic(i))
 			{
-				mainmenu_node::ptr node = v2_ptr->dynamic_instantiate(i);
+				const auto node = v2_ptr->dynamic_instantiate(i);
 				if (execute_recur(node, parent_path))
 					return true;
 			}
@@ -73,7 +73,7 @@ bool MainMenuCommand::execute()
 			{
 				string8 name;
 				ptr->get_name(i, name);
-				const std::string path = parent_path + name.get_ptr();
+				const auto path = parent_path + name.get_ptr();
 
 				if (match_command(path))
 				{
@@ -95,36 +95,32 @@ bool MainMenuCommand::execute_recur(mainmenu_node::ptr node, std::string_view pa
 	uint32_t flags{};
 	node->get_display(text, flags);
 
+	const auto type = node->get_type();
 	auto path = fmt::format("{}{}", parent_path, text.get_ptr());
 
-	switch (node->get_type())
+	if (type == mainmenu_node::type_group)
 	{
-	case mainmenu_node::type_group:
+		if (!path.ends_with("/"))
 		{
-			if (!path.ends_with("/"))
-			{
-				path.append("/");
-			}
-
-			for (const size_t i : std::views::iota(size_t{}, node->get_children_count()))
-			{
-				mainmenu_node::ptr child = node->get_child(i);
-				if (execute_recur(child, path))
-					return true;
-			}
-			break;
+			path.append("/");
 		}
-	case mainmenu_node::type_command:
-		if (match_command(path))
+
+		for (const size_t i : std::views::iota(size_t{}, node->get_children_count()))
 		{
-			if (WI_IsFlagSet(flags, mainmenu_commands::flag_disabled))
-				return false;
-
-			node->execute(nullptr);
-			return true;
+			const auto child = node->get_child(i);
+			if (execute_recur(child, path))
+				return true;
 		}
-		break;
 	}
+	else if (type == mainmenu_node::type_command && match_command(path))
+	{
+		if (WI_IsFlagSet(flags, mainmenu_commands::flag_disabled))
+			return false;
+
+		node->execute(nullptr);
+		return true;
+	}
+
 	return false;
 }
 
